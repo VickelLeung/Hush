025a32df13111cd120ae7a636a72e4988a928513
vickel leung <>
2020-02-13 23:36:52 -0500

    added client

--- client/src/images/backgroundImages/index.js
+++
@@ -1,4 +0,0 @@
-export const one = require('./aaron-burden--IQGXSMLYJc-unsplash.jpg');
-export const two = require('./hussain-faruhaan-XOEL0hNDub0-unsplash.jpg');
-// export const three = require('./joao-marcelo-martins-hFoPRRwjVgg-unsplash.jpg');
-export const four = require('./motoki-tonn-vV1a1Leq-dQ-unsplash.jpg');
