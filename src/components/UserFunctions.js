4d3371df986dd75d3541f8050973c199b9e695cd
vickel leung <>
2020-02-18 19:02:12 -0500

    updated client UI

--- src/components/UserFunctions.js
+++ src/components/UserFunctions.js
@@ -1,40 +1,43 @@
 import axios from "axios";
 
-export const register = (newUser) =>{
-    return axios.post("https://hushbackend.herokuapp.com/user/register",{
-        displayName: newUser.displayName,
-        email: newUser.email,
-        password: newUser.password
+export const register = newUser => {
+  return axios
+    .post("https://hushbackend.herokuapp.com/user/register", {
+      displayName: newUser.displayName,
+      email: newUser.email,
+      password: newUser.password
     })
-    .then(response =>{
-        console.log("registered");
-        console.log(newUser);
-    })
-}
+    .then(res => {
+      //   console.log("registered");
+      //   console.log(newUser);
+      //   console.log(res);
+      return res;
+    });
+};
 
-export const login = (user) =>{
-    return axios.post("https://hushbackend.herokuapp.com/user/login",{
-        email: user.email,
-        password: user.password
+export const login = user => {
+  return axios
+    .post("https://hushbackend.herokuapp.com/user/login", {
+      email: user.email,
+      password: user.password
     })
-    .then(response =>{
-        localStorage.setItem("usertoken", response.data)
-        return response.data;
+    .then(response => {
+      localStorage.setItem("usertoken", response.data);
+      return response.data;
     })
-    .catch(err =>{
-        console.log("Error " + err);
-    })
-}
+    .catch(err => {
+      console.log("Error " + err);
+    });
+};
 
-export const getProfile = user =>{
-    return axios.get("https://hushbackend.herokuapp.com/user/profile",{
-
+export const getProfile = user => {
+  return axios
+    .get("https://hushbackend.herokuapp.com/user/profile", {})
+    .then(response => {
+      console.log(response);
+      return response.data;
     })
-    .then(response=>{
-        console.log(response);
-        return response.data;
-    })
-    .catch(err =>{
-        console.log(err);
-    })
-}
+    .catch(err => {
+      console.log(err);
+    });
+};
--- src/pages/Authenticate.js
+++ src/pages/Authenticate.js
@@ -1,101 +1,138 @@
 import React, { PureComponent } from "react";
 import styled from "styled-components";
 import Button from "@material-ui/core/Button";
-import TextField from '@material-ui/core/TextField';
-import {login} from "../components/UserFunctions";
-import {register} from "../components/UserFunctions";
+import TextField from "@material-ui/core/TextField";
+import { login } from "../components/UserFunctions";
+import { register } from "../components/UserFunctions";
 
-class Authenticate extends PureComponent{
-    state={
-        isLogin: false,
-        isAuthenticated: false,
-        loginEmail:"",
-        loginPassword:"",
-        
-        isRegistered: false,
-        displayName: "",
-        registerEmail:"",
-        registerPassword:""
-    }
+class Authenticate extends PureComponent {
+  state = {
+    isLogin: false,
+    isAuthenticated: false,
+    loginEmail: "",
+    loginPassword: "",
 
-    submitRegister =()=>{
-        const userData = { 
-            email: this.state.registerEmail,
-            displayName : this.state.displayName === 0 ? this.state.displayName  : "no-name",
-            password: this.state.registerPassword
-        }
+    isRegistered: false,
+    displayName: "",
+    registerEmail: "",
+    registerPassword: ""
+  };
 
-        console.log(userData);
+  submitRegister = () => {
+    const userData = {
+      email: this.state.registerEmail,
+      displayName:
+        this.state.displayName === 0 ? this.state.displayName : "no-name",
+      password: this.state.registerPassword
+    };
 
-        register(userData).then(res => {
-            if (res) {
-                console.log("good");
-              this.props.history.push(`/login`)
-            }
-            else{
-                console.log("bad");
-            }
-          })
-    }
+    console.log(userData);
 
-    submitLogin =()=>{
-      const userData = {
-          email: this.state.loginEmail,
-          password: this.state.loginPassword
+    register(userData).then(res => {
+      console.log(res);
+      if (res) {
+        console.log(res);
+        console.log("good");
+        this.props.history.push(`/login`);
+      } else {
+        console.log("bad");
       }
+    });
+  };
 
-      login(userData).then(res => {
-        if (res) {
-          this.props.history.push(`/profile`)
-        }
-      })
-    }
+  submitLogin = () => {
+    const userData = {
+      email: this.state.loginEmail,
+      password: this.state.loginPassword
+    };
 
-    render(){
-    let Login = <LoginContainer>
+    login(userData).then(res => {
+      if (res) {
+        this.props.history.push(`/profile`);
+      }
+    });
+  };
+
+  render() {
+    let Login = (
+      <LoginContainer>
         <Title>Login</Title>
         {/* {this.state.isLogin ? <p>You have successfully logged in</p> : <p>Error, please try again!</p>} */}
         <FormContainer>
-            <TextField onChange={(e)=>{this.setState({loginEmail: e.target.value})}} label="Email: "/>
-            <TextField type="password" onChange={(e)=>{this.setState({loginPassword: e.target.value})}} label="Password"/>
-            <Button onClick={this.submitLogin}>Submit</Button>
+          <TextField
+            onChange={e => {
+              this.setState({ loginEmail: e.target.value });
+            }}
+            label="Email: "
+          />
+          <TextField
+            type="password"
+            onChange={e => {
+              this.setState({ loginPassword: e.target.value });
+            }}
+            label="Password"
+          />
+          <Button onClick={this.submitLogin}>Submit</Button>
         </FormContainer>
         <LinkContainer>
-            <Button>Forgot password?</Button>
-            <Button onClick={()=>{this.setState({isAuthenticated: !this.state.isAuthenticated})}}>
-                Register an account
-            </Button>
+          <Button>Forgot password?</Button>
+          <Button
+            onClick={() => {
+              this.setState({ isAuthenticated: !this.state.isAuthenticated });
+            }}
+          >
+            Register an account
+          </Button>
         </LinkContainer>
-    </LoginContainer>
+      </LoginContainer>
+    );
 
-    let Register = <div>
+    let Register = (
+      <div>
         <Title>Register</Title>
         {/* {this.state.isRegistered ? <p>You have successfully registered</p> : <p>Error, please try again!</p>} */}
         <FormContainer>
-            <TextField onChange={(e)=>{this.setState({registerEmail: e.target.value})}} label="Email: "/>
-            <TextField onChange={(e)=>{this.setState({displayName: e.target.value})}} label="Display name: "/>
-            <TextField type="password" onChange={(e)=>{this.setState({registerPassword: e.target.value})}} label="Password"/>
-            <Button onClick={this.submitRegister}>Submit</Button>
+          <TextField
+            onChange={e => {
+              this.setState({ registerEmail: e.target.value });
+            }}
+            label="Email: "
+          />
+          <TextField
+            onChange={e => {
+              this.setState({ displayName: e.target.value });
+            }}
+            label="Display name: "
+          />
+          <TextField
+            type="password"
+            onChange={e => {
+              this.setState({ registerPassword: e.target.value });
+            }}
+            label="Password"
+          />
+          <Button onClick={this.submitRegister}>Submit</Button>
         </FormContainer>
         <LinkContainer>
-                <Button onClick={()=>{this.setState({isAuthenticated: !this.state.isAuthenticated})}}>
-                    Already have an account? Click to Login
-                </Button>
+          <Button
+            onClick={() => {
+              this.setState({ isAuthenticated: !this.state.isAuthenticated });
+            }}
+          >
+            Already have an account? Click to Login
+          </Button>
         </LinkContainer>
-    </div>
+      </div>
+    );
 
-    return(
-        <Wrapper>
-           {this.state.isAuthenticated ? Login : Register}
-        </Wrapper>
-    )
-    }
+    return <Wrapper>{this.state.isAuthenticated ? Login : Register}</Wrapper>;
+  }
 }
 
-export {Authenticate};
+export { Authenticate };
 
 const Wrapper = styled.div`
-text-align: center;
+  text-align: center;
 `;
 
 const LoginContainer = styled.div``;
