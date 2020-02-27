80e9cbfa4c430af4079e8b2877944329dd0b8cc0
vickel leung <>
2020-02-27 13:19:02 -0500

    updated search pages UI/UX

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -18,15 +18,22 @@ class SearchBar extends PureComponent {
     });
   };
 
+  onKeyPress = e => {
+    if (e.which === 13) {
+      this.SubmitSearch();
+    }
+  };
+
   render() {
     return (
       <SearchContainer>
-        <InputBase
+        <InputContainer
           placeholder="Search posts"
+          onKeyPress={this.onKeyPress}
           onChange={e => {
             this.setState({ searchInput: e.target.value });
           }}
-        />
+        ></InputContainer>
         <IconButton type="submit" aria-label="search">
           <SearchIcon onClick={this.SubmitSearch} />
         </IconButton>
@@ -41,4 +48,14 @@ const SearchContainer = styled.div`
   text-align: right;
   margin: 2%; 10%;
 
+`;
+
+const InputContainer = styled(InputBase)`
+  border: 1px solid gray;
+  padding: 0 1%;
+    border-radius: 5px;
+  &:hover {
+    border: 1px solid black;
+  }
+  }
 `;
--- src/pages/Search.js
+++ src/pages/Search.js
@@ -3,18 +3,25 @@ import React, { PureComponent } from "react";
 import { searchInput } from "../components/UserFunctions";
 // import Card from '@material-ui/core/Card';
 // import Typography from '@material-ui/core/Typography';
+import Highlighter from "react-highlight-words";
+import Card from "@material-ui/core/Card";
+import Typography from "@material-ui/core/Typography";
+import styled from "styled-components";
+import { Link } from "react-router-dom";
+
+import { Redirect } from "react-router";
 
 class Search extends PureComponent {
-  state = { userInfo: [], title: [], description: [] };
+  state = { param: "", userInfo: [], title: [], description: [] };
 
   componentDidMount = () => {
-    // let getProps = this.props.location.state.input;
     let getProps = this.props.match.params.param;
 
     console.log("getProps: " + getProps);
     searchInput(getProps)
       .then(res => {
         this.setState({
+          param: getProps,
           userInfo: res,
           title: res[0],
           description: res[1]
@@ -32,28 +39,88 @@ class Search extends PureComponent {
 
   render() {
     return (
-      <div>
-        <h1>Here are your search results</h1>
+      <Wrapper>
+        <h1>Your search results for: "{this.state.param}"</h1>
         <h1>Title:</h1>
-        <div>
-          {this.state.title.map(item => {
-            return <p>{item.title}</p>;
-          })}
-        </div>
-      </div>
+
+        <CardContainer>
+          {this.state.title ? (
+            this.state.title.map(item => {
+              return (
+                <LinkItem to={"/categories/" + item.category + "/" + item._id}>
+                  <CardItem>
+                    <Typography variant="h3">
+                      <Highlighter
+                        searchWords={[this.state.param]}
+                        textToHighlight={item.title}
+                      />
+                    </Typography>
+                    <Typography variant="h4">
+                      Category: {item.category}
+                    </Typography>
+                    <Typography variant="h4">From: {item.user}</Typography>
+                    <Typography variant="h5">{item.date}</Typography>
+                    <Typography variant="h5">{item.description}</Typography>
+                  </CardItem>
+                </LinkItem>
+              );
+            })
+          ) : (
+            <p>No search founds with title</p>
+          )}
+        </CardContainer>
+        <h1>Description</h1>
+
+        <CardContainer>
+          {this.state.description ? (
+            this.state.description.map(item => {
+              return (
+                <LinkItem to={"/categories/" + item.category + "/" + item._id}>
+                  <CardItem>
+                    <Typography variant="h3">{item.title}</Typography>
+                    <Typography variant="h4">From: {item.category}</Typography>
+                    <Typography variant="h4">From: {item.user}</Typography>
+                    <Typography variant="h5">{item.date}</Typography>
+                    <Typography variant="h5">
+                      <Highlighter
+                        searchWords={[this.state.param]}
+                        textToHighlight={item.description}
+                      />
+                    </Typography>
+                  </CardItem>
+                </LinkItem>
+              );
+            })
+          ) : (
+            <p>No search founds with description</p>
+          )}
+        </CardContainer>
+      </Wrapper>
     );
   }
 }
 
-// {this.state.title.map(item => {
-//   return (
-//     <div>
-//       <p>{item.title}</p>
-//       <p>{item.category}</p>
-//       <p>{item.description}</p>
-//       <p>{item.user}</p>
-//     </div>
-//   );
-// })}
-
 export { Search };
+
+const Wrapper = styled.div`
+  text-align: left;
+  margin: 0 25%;
+`;
+
+const CardContainer = styled.div``;
+
+const CardItem = styled(Card)`
+  margin: 5% 0;
+  padding: 4%;
+`;
+
+const LinkItem = styled(Link)`
+  text-decoration: none;
+  color: black;
+  flex: 0 0 50%;
+
+  &:hover {
+    background: rgba(0, 0, 0, 1);
+    color: red;
+  }
+`;
